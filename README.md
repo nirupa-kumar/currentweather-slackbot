281101a87d81d3222fcfec5d2377922c8670fddd
nirupa-kumar <nirupakumar@google.com>
2018-10-01 16:17:48 -0700

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # currentweather-slackbot
+
+Gets the current weather conditions for a given city.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+### Prerequisites
+
+
+Will need to have both Node and npm installed. 
+
+
+### Installing
+
+Clone the repository to your local machine
+
+```
+git clone https://github.com/nirupa-kumar/currentweather-slackbot.git
+```
+Open the root directory and run the following command
+
+```
+npm install
+```
+
+You should observe similar output as shown below if everything has been set up correctly
+
+```
+```
+## Running the tests
+
+Currently haven’t set up any automation tests. - Needs to be worked on. 
+
+### And coding style tests
+
+Would like to use ESlint & prettier for this 
+
+## Deployment
+
+This app has been deployed on heroku a
+
+## Built With
+
+## References
---
+++ index.js
@@ -0,0 +1,257 @@
+/**
+ * A Bot for Slack!
+ */
+
+
+/**
+ * Define a function for initiating a conversation on installation
+ * With custom integrations, we don't have a way to find out who installed us, so we can't message them :(
+ */
+const argv = require('yargs').argv;
+let request = require('request');
+
+function onInstallation(bot, installer) {
+    if (installer) {
+        bot.startPrivateConversation({user: installer}, function (err, convo) {
+            if (err) {
+                console.log(err);
+            } else {
+                convo.say('I am a bot that has just joined your team');
+                convo.say('You must now /invite me to a channel so that I can be of use!');
+            }
+        });
+    }
+}
+
+
+/**
+ * Configure the persistence options
+ */
+
+var config = {};
+if (process.env.MONGOLAB_URI) {
+    var BotkitStorage = require('botkit-storage-mongo');
+    config = {
+        storage: BotkitStorage({mongoUri: process.env.MONGOLAB_URI}),
+    };
+} else {
+    config = {
+        json_file_store: ((process.env.TOKEN)?'./db_slack_bot_ci/':'./db_slack_bot_a/'), //use a different name if an app or CI
+    };
+}
+
+/**
+ * Are being run as an app or a custom integration? The initialization will differ, depending
+ */
+
+if (process.env.TOKEN || process.env.SLACK_TOKEN) {
+    //Treat this as a custom integration
+    var customIntegration = require('./lib/custom_integrations');
+    var token = (process.env.TOKEN) ? process.env.TOKEN : process.env.SLACK_TOKEN;
+    var controller = customIntegration.configure(token, config, onInstallation);
+} else if (process.env.CLIENT_ID && process.env.CLIENT_SECRET && process.env.PORT) {
+    //Treat this as an app
+    var app = require('./lib/apps');
+    var controller = app.configure(process.env.PORT, process.env.CLIENT_ID, process.env.CLIENT_SECRET, config, onInstallation);
+} else {
+    console.log('Error: If this is a custom integration, please specify TOKEN in the environment. If this is an app, please specify CLIENT_ID, CLIENT_SECRET, and PORT in the environment');
+    process.exit(1);
+}
+
+
+/**
+ * A demonstration for how to handle websocket events. In this case, just log when we have and have not
+ * been disconnected from the websocket. In the future, it would be super awesome to be able to specify
+ * a reconnect policy, and do reconnections automatically. In the meantime, we aren't going to attempt reconnects,
+ * WHICH IS A B0RKED WAY TO HANDLE BEING DISCONNECTED. So we need to fix this.
+ *
+ * TODO: fixed b0rked reconnect behavior
+ */
+// Handle events related to the websocket connection to Slack
+controller.on('rtm_open', function (bot) {
+    console.log('** The RTM api just connected!');
+});
+
+controller.on('rtm_close', function (bot) {
+    console.log('** The RTM api just closed');
+    // you may want to attempt to re-open
+});
+
+
+/**
+ * Core bot logic goes here!
+ */
+// BEGIN EDITING HERE!
+
+controller.on('bot_channel_join', function (bot, message) {
+    bot.reply(message, "I'm here!")
+});
+
+controller.hears('hello', 'direct_message', function (bot, message) {
+    bot.reply(message, 'Hello!');
+});
+
+controller.hears(['weather (.*)','(.*) weather','weather in (.*)','weather at (.*)'], 'direct_message', function (bot, message) {
+    let city = message.match[1];
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=${process.env.OPEN_WEATHER_API_KEY}`;
+    //bot.reply(message, `Bringing it right up! The weather for ${city}`);
+    request(url, function(err,response,body){
+
+        if(err){
+            console.log("error: ",err);
+            bot.reply(message,`Sorry had some trouble getting the weather. Please go ahead and try again.`);
+        } else {
+            let weather = JSON.parse(body);
+            //let message =`It's ${weather.main.temp} degrees in ${weather.name}!`;
+            //console.log(message);
+            bot.reply(message, `It's ${weather.main.temp} degrees Celsius right now in ${weather.name}!`);
+        }
+
+    });
+});
+
+
+/**
+ * AN example of what could be:
+ * Any un-handled direct mention gets a reaction and a pat response!
+ */
+//controller.on('direct_message,mention,direct_mention', function (bot, message) {
+//    bot.api.reactions.add({
+//        timestamp: message.ts,
+//        channel: message.channel,
+//        name: 'robot_face',
+//    }, function (err) {
+//        if (err) {
+//            console.log(err)
+//        }
+//        bot.reply(message, 'I heard you loud and clear boss.');
+//    });
+//});
+/**
+ * A Bot for Slack!
+ */
+
+
+/**
+ * Define a function for initiating a conversation on installation
+ * With custom integrations, we don't have a way to find out who installed us, so we can't message them :(
+ */
+const argv = require('yargs').argv;
+let request = require('request');
+
+function onInstallation(bot, installer) {
+    if (installer) {
+        bot.startPrivateConversation({user: installer}, function (err, convo) {
+            if (err) {
+                console.log(err);
+            } else {
+                convo.say('I am a bot that has just joined your team');
+                convo.say('You must now /invite me to a channel so that I can be of use!');
+            }
+        });
+    }
+}
+
+
+/**
+ * Configure the persistence options
+ */
+
+var config = {};
+if (process.env.MONGOLAB_URI) {
+    var BotkitStorage = require('botkit-storage-mongo');
+    config = {
+        storage: BotkitStorage({mongoUri: process.env.MONGOLAB_URI}),
+    };
+} else {
+    config = {
+        json_file_store: ((process.env.TOKEN)?'./db_slack_bot_ci/':'./db_slack_bot_a/'), //use a different name if an app or CI
+    };
+}
+
+/**
+ * Are being run as an app or a custom integration? The initialization will differ, depending
+ */
+
+if (process.env.TOKEN || process.env.SLACK_TOKEN) {
+    //Treat this as a custom integration
+    var customIntegration = require('./lib/custom_integrations');
+    var token = (process.env.TOKEN) ? process.env.TOKEN : process.env.SLACK_TOKEN;
+    var controller = customIntegration.configure(token, config, onInstallation);
+} else if (process.env.CLIENT_ID && process.env.CLIENT_SECRET && process.env.PORT) {
+    //Treat this as an app
+    var app = require('./lib/apps');
+    var controller = app.configure(process.env.PORT, process.env.CLIENT_ID, process.env.CLIENT_SECRET, config, onInstallation);
+} else {
+    console.log('Error: If this is a custom integration, please specify TOKEN in the environment. If this is an app, please specify CLIENTID, CLIENTSECRET, and PORT in the environment');
+    process.exit(1);
+}
+
+
+/**
+ * A demonstration for how to handle websocket events. In this case, just log when we have and have not
+ * been disconnected from the websocket. In the future, it would be super awesome to be able to specify
+ * a reconnect policy, and do reconnections automatically. In the meantime, we aren't going to attempt reconnects,
+ * WHICH IS A B0RKED WAY TO HANDLE BEING DISCONNECTED. So we need to fix this.
+ *
+ * TODO: fixed b0rked reconnect behavior
+ */
+// Handle events related to the websocket connection to Slack
+controller.on('rtm_open', function (bot) {
+    console.log('** The RTM api just connected!');
+});
+
+controller.on('rtm_close', function (bot) {
+    console.log('** The RTM api just closed');
+    // you may want to attempt to re-open
+});
+
+
+/**
+ * Core bot logic goes here!
+ */
+// BEGIN EDITING HERE!
+
+controller.on('bot_channel_join', function (bot, message) {
+    bot.reply(message, "I'm here!")
+});
+
+controller.hears('hello', 'direct_message', function (bot, message) {
+    bot.reply(message, 'Hello!');
+});
+
+controller.hears(['weather (.*)','(.*) weather'], 'direct_message', function (bot, message) {
+    let city = message.match[1];
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=${process.env.OPEN_WEATHER_API_KEY}`;
+    //bot.reply(message, `Bringing it right up! The weather for ${city}`);
+    request(url, function(err,response,body){
+
+        if(err){
+            console.log("error: ",err);
+        } else {
+            let weather = JSON.parse(body);
+            //let message =`It's ${weather.main.temp} degrees in ${weather.name}!`;
+            //console.log(message);
+            bot.reply(message, `It's ${weather.main.temp} degrees right now in ${weather.name}!`);
+        }
+
+    });
+});
+
+
+/**
+ * AN example of what could be:
+ * Any un-handled direct mention gets a reaction and a pat response!
+ */
+//controller.on('direct_message,mention,direct_mention', function (bot, message) {
+//    bot.api.reactions.add({
+//        timestamp: message.ts,
+//        channel: message.channel,
+//        name: 'robot_face',
+//    }, function (err) {
+//        if (err) {
+//            console.log(err)
+//        }
+//        bot.reply(message, 'I heard you loud and clear boss.');
+//    });
+//});
